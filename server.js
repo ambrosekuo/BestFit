ab63d2f1445263f6a695a4987bb0e23fff030566
ambrose214 <ambrose.kuo@hotmail.com>
2019-03-21 22:45:36 -0400

    updates to deploy heroku 2

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname));
+app.use(express.static('./dist/fitnessGame'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
+res.sendFile(path.join(__dirname,'/dist/fitnessGame/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku
