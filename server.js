b1e6f44fcbd4cd0214385b825eaff9a2f1552f50
ambrose214 <ambrose.kuo@hotmail.com>
2019-03-25 01:49:24 -0400

    init angular heroku

--- server.js
+++ server.js
@@ -12,4 +12,5 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/fitnessGame/index.html'));
 });
 
+app.listen(process.env.PORT || 8080);
 // Start the app by listening on the default Heroku
